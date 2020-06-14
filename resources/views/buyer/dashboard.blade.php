4abd5d86b58d177d75f575c0147d4254fdc1335e
Romel Indemne <romel.i@gagroup.net>
2020-06-14 16:56:09 +0400

    Frontend -- Added Buyer's pages

--- resources/views/buyer/dashboard.blade.php
+++ resources/views/buyer/dashboard.blade.php
@@ -1,27 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
+{{-- this should be SPA --}}
 <div class="container">
     <div class="row">
         <div class="col-md-3">
-            <div class="shadow-sm px-3 py-4 bg-white rounded">
-                <h6 class="text-uppercase">Dashboard</h6>
-            </div>
+            <a href="{{ route('buyer.dashboard') }}" style="text-decoration: none;">
+                <div class="shadow-sm px-3 py-4 bg-white rounded">
+                    <h6 class="text-uppercase mb-0">Dashboard</h6>
+                </div>
+            </a>
         </div>
         <div class="col-md-3">
-            <div class="shadow-sm px-3 py-4 bg-white rounded">
-                <h6 class="text-uppercase">Orders</h6>
-            </div>
+            <a href="{{ route('buyer.orders') }}" style="text-decoration: none;">
+                <div class="shadow-sm px-3 py-4 bg-white rounded d-flex justify-content-between">
+                    <h6 class="text-uppercase mb-0">Orders</h6>
+                    <span class="badge badge-primary badge-pill">14</span>
+                </div>
+            </a>
         </div>
         <div class="col-md-3">
-            <div class="shadow-sm px-3 py-4 bg-white rounded">
-                <h6 class="text-uppercase">Inquiries</h6>
-            </div>
+            <a href="{{ route('buyer.inquiries') }}" style="text-decoration: none;">
+                <div class="shadow-sm px-3 py-4 bg-white rounded">
+                    <h6 class="text-uppercase mb-0">Inquiries</h6>
+                </div>
+            </a>
         </div>
         <div class="col-md-3">
-            <div class="shadow-sm px-3 py-4 bg-white rounded">
-                <h6 class="text-uppercase">Messages</h6>
-            </div>
+            <a href="{{ route('buyer.messages') }}" style="text-decoration: none;">
+                <div class="shadow-sm px-3 py-4 bg-white rounded d-flex justify-content-between">
+                    <h6 class="text-uppercase mb-0">Messages</h6>
+                    <span class="badge badge-primary badge-pill">9</span>
+                </div>
+            </a>
         </div>
     </div>
 </div>
