503344cfdb18b6f5e2c71592207fd8fdae9cc0b2
Romel Indemne <romel.i@gagroup.net>
2020-07-05 15:43:43 +0400

    Seller -- updated layouts

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -63,41 +63,6 @@
                     </ul>
                 </div>
             </nav>
-
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}" style="line-height: 60px;">
-                        MARKETPLACE
-                        {{-- <img src="https://aboudcrm.com/store/wp-content/uploads/2020/04/olive-country.png"
-                            alt="Olive Country" width="100"> --}}
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto"></ul>
-                        <!-- Right Side Of Navbar -->
-                        <ul class="nav justify-content-end">
-                            <li class="nav-item">
-                                <a class="nav-link active text-dark" href="{{ route('home') }}">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-dark" href="{{ route('products') }}">Products</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-dark" href="{{ route('about') }}">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-dark" href="{{ route('contact') }}">Contact us</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>                
-      
         </header>
 
         <main class="py-4" style="min-height:85vh;">
